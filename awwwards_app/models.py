e43eef2e5fbe1ee38a36c0eb4b0a664fc65d1199
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-12 02:03:29 +0300

    Add the Profile model

--- awwwards_app/models.py
+++ awwwards_app/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
-from django.forms import CharField
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    profile_pic = CloudinaryField('image')
+    full_name = models.CharField(max_length = 50)
+    bio = models.TextField()
+    user = models.OneToOneField(User, on_delete = models.CASCADE, related_name = 'profile')
+
+    def __str__(self):
+        return self.full_name
+
+
 class Project(models.Model):
     title = models.CharField(max_length = 100)
     image = CloudinaryField('image')
     description = models.TextField()
     live_link = models.URLField(max_length = 200)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'projects')
+
+    def __str__(self):
+        return self.title
