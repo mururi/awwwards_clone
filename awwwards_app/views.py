c52719a104836f7ae5a4a55d23ff83f5ce9a829c
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-11 12:32:30 +0300

    Add Registration form template

--- awwwards_app/views.py
+++ awwwards_app/views.py
@@ -1,8 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
+from .forms import RegisterForm
 
-# Create your views here.
+
 def home(request):
     projects = Project.objects.all()
     return render(request, 'index.html', {"projects": projects})
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # username = form.cleaned_data.get('username')
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/register.html', {"form": form})
