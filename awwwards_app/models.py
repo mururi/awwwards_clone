9cd59756c53d821f1edcc37c758f7f5f55ec0e82
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-12 02:17:42 +0300

    Add Rate model

--- awwwards_app/models.py
+++ awwwards_app/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Profile(models.Model):
     profile_pic = CloudinaryField('image')
     full_name = models.CharField(max_length = 50)
     bio = models.TextField()
-    user = models.OneToOneField(User, on_delete = models.CASCADE, related_name = 'profile')
+    user = models.OneToOneField(User, on_delete = models.CASCADE, related_name = 'profile', null = True)
 
     def __str__(self):
         return self.full_name
@@ -18,7 +19,14 @@ class Project(models.Model):
     image = CloudinaryField('image')
     description = models.TextField()
     live_link = models.URLField(max_length = 200)
-    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'projects')
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'projects', null = True)
 
     def __str__(self):
         return self.title
+
+class Rate(models.Model):
+    design = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
+    usability = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
+    creativity = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
+    project = models.ForeignKey(Project, on_delete = models.CASCADE, null = True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null = True)
