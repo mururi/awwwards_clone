a93387184b7f896034eb0861ea95a9e042545ce8
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-12 06:19:38 +0300

    Add Project CRUD methods

--- awwwards_app/models.py
+++ awwwards_app/models.py
@@ -37,6 +37,23 @@ class Project(models.Model):
     live_link = models.URLField(max_length = 200)
     user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'projects', null = True)
 
+    def save_project(self):
+        self.save()
+
+    @classmethod
+    def delete_project(cls, id):
+        '''
+        Method to delete a Project object
+        '''
+        cls.objects.filter(id = id).delete()
+
+    @classmethod
+    def get_project_by_id(cls, id):
+        '''
+        Method to get a Project by its id
+        '''
+        return cls.objects.filter(id = id)[0]
+
     def __str__(self):
         return self.title
 
